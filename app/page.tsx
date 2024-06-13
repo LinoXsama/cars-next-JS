a0c5456cf7c8630d9efa9fd3d14d2354eaa5446e
Merlin Migan <merlin.migan@gmail.com>
2024-06-13 16:41:44 +0200

    Création des composants SearchBar et SearchManufacturer !

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
-import { Hero } from "@/components";
-// import Image from "next/image";
+import { Hero, SearchBar } from "@/components";
+
 
 export default function Home() {
   return (
-    <main className="overflow-hidden">
+    <main className='overflow-hidden'>
       <Hero />
+
+      <div className='mt-12 padding-x padding-y' id="discover">
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>
+            Car Catalogue
+          </h1>
+          <p>
+            Explore the cars you might like
+          </p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            {/* Ici sera : <CustomFilter title="full" /> */}
+            {/* Ici sera : <CustomFilter title="year" /> */}
+          </div>
+        </div>
+      </div>
     </main>
   );
-}
+};
--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,15 @@
-import Hero from './Hero';
-import CustomButton from './CustomButton';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+// import SearchManufacturer from "./SearchManufacturer";
 
-export {
-    Hero,
-    CustomButton,
-    Navbar,
-    Footer
+export { 
+  Hero,
+  CustomButton, 
+  Navbar,
+  Footer,
+  SearchBar,
+  // SearchManufacturer
 };
