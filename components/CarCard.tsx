246a6d8c4c2c7407789ec04f7d84c916662fb9ae
Merlin Migan <merlin.migan@gmail.com>
2024-06-24 12:27:13 +0200

    Création de la fonction generateCarImageUrl qui permet de générer des images via l'API de Imagine Studio et modification des composants CarCard et CarDetails !

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -2,13 +2,14 @@
 
 import { CarProps } from "@/types";
 
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 
 import Image from "next/image";
 
 import { useState } from "react";
 
 import { CustomButton, CarDetails } from "@/components";
+
 
 interface CarCardProps {
   car: CarProps;
@@ -37,7 +38,7 @@ const CarCard = ({ car }: CarCardProps) => {
 
       <div className="relative w-full h-40 my-3">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -13,6 +13,8 @@ import {
   DialogPanel,
 } from "@headlessui/react";
 
+import { generateCarImageUrl } from "@/utils";
+
 interface CarDetailsProps {
   isOpen: boolean;
   closeModal: () => void;
@@ -63,7 +65,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                   <div className='flex-1 flex flex-col gap-3'>
                     <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg' >
                       <Image
-                        src="/hero.png"
+                        src={generateCarImageUrl(car)}
                         alt="car model"
                         fill priority
                         className='object-contain'
@@ -73,7 +75,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <div className='flex gap-3'>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg' >
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, '29')}
                           alt="car model"
                           fill priority
                           className='object-contain'
@@ -81,7 +83,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       </div>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg' >
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, '33')}
                           alt="car model"
                           fill priority
                           className='object-contain'
@@ -89,7 +91,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       </div>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg' >
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, '13')}
                           alt="car model"
                           fill priority
                           className='object-contain'
